26ea473c4eb60e1417d2d94b6b9e0026740cf430
Sakthi3015q <sakthimuruganv2005@gmail.com>
2025-10-01 11:43:09 +0530

    Add files via upload

---
+++ Behavioural/Stereo.java
@@ -0,0 +1,16 @@
+public class Stereo implements Device{
+    @Override
+    public void turnOn() {
+        System.out.println("Stereo is now ON");
+
+    }
+
+    @Override
+    public void turnOff() {
+        System.out.println("Stereo is now OFF");
+
+    }
+    public void adjustVolume(){
+        System.out.println("Volume is adjusted");
+    }
+}
---
+++ Behavioural/TurnOnCommand.java
@@ -0,0 +1,10 @@
+public class TurnOnCommand implements Command{
+    Device device;
+    public  TurnOnCommand(Device device){
+        this.device=device;
+    }
+    @Override
+    public void execute() {
+           device.turnOn();
+    }
+}
